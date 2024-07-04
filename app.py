3bc2f19949f8cd68c945e31a520fce8785613f5f
Sahil Chandel <106423652+Sahil-Chandel@users.noreply.github.com>
2024-07-04 11:03:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+import logging
+
+
+## logging setting
+
+
+logging.basicConfig(
+                     level=logging.DEBUG,
+                     format = '%(asctime)s-%(levelname)s-%(message)s',
+                     datefmt = '%Y-%m-%d %H:%M:%S',
+                     handlers=[
+                         
+                         logging.FileHandler("app1.log"),
+                         logging.StreamHandler()
+                     ]
+)
+
+logger=logging.getLogger("ArithmaticApp")
+
+def add(a, b):
+    result=a+b
+    logger.debug(f"Adding {a} + {b} = {result}")
+    return result
+
+def subtrsct(a, b):
+    result=a-b
+    logger.debug(f"Subtracting {a} - {b} = {result}")
+    return result
+
+def multiply(a, b):
+    result= a * b
+    logger.debug(f"multiply {a} * {b} = {result}")
+    return result
+
+def divide(a, b):
+    try:
+        result=a / b
+        logger.debug(f"Dividing {a} / {b} = {result}")
+        return result
+    except ZeroDivisionError:
+        logger.error("Division by zero error")
+        return None
+    
+add(10,15)
+subtrsct(15,10)
+multiply(10,20)
+divide(20,0)
