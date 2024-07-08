e3cc814f7f51b67b0a553564478cdc38668a2f1f
Sahil Chandel <106423652+Sahil-Chandel@users.noreply.github.com>
2024-07-08 16:08:41 +0530

    Add files via upload

---
+++ getpost.py
@@ -0,0 +1,50 @@
+### Integrate HTML with Flask
+## HTTP verb GET and post
+
+from flask import Flask,redirect,url_for,render_template,request
+'''
+It creates an instance of the flask class,
+which will be your WSGI(Web Server Gateway Interface) application.
+'''
+### WSGI Application
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><H1>Welcome to the flask day 4 </H1></html>"
+    
+
+
+
+@app.route("/index",methods=['GET'])
+def index():
+    return render_template('index.html')
+    
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask,redirect,url_for
+### Integrate HTML with Flask
+## HTTP verb GET and post
+
+from flask import Flask,redirect,url_for,render_template
 '''
 It creates an instance of the flask class,
 which will be your WSGI(Web Server Gateway Interface) application.
@@ -8,15 +11,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "welcome to flask  best Hey sahil"
+    return render_template('index.html')
+
+
 
 @app.route('/success/<int:score>')
 def success(score):
     return "The Person has passes and the mark is "+ str(score)
 
+
+
 @app.route('/fail/<int:score>')
 def fail(score):
     return "The Person has failed and the mark is "+ str(score)
+
+
 
 ###   Result checker 
 @app.route('/results/<int:marks>')
